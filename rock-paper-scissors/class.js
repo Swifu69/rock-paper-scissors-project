d6bdab96a7148cb38d1ef6b187e48315fe9c3601
swifuCS <xlrgameryt04d@gmail.com>
2021-10-14 22:53:08 +0200

    Jeg hater denne oppgaven

--- rock-paper-scissors/class.js
+++ rock-paper-scissors/class.js
@@ -1,16 +1,14 @@
-
-export class Choice{
-    #name 
-    #Weakness
-    constructor(name, weakness) {
-        this.#name = name;
-        this.#weakness = beaten;
-    }
-    get name(){
-        return this.#name.toLowerCase().trim();
-
-    }
-    get weakness(){
-        return this.#weakness.toLowerCase().trim();
-    }
+export class Item {
+  #name;
+  #weakness;
+  constructor(name, weakness) {
+    this.#name = name;
+    this.#weakness = weakness;
+  }
+  get name() {
+    return this.#name.toLowerCase().trim();
+  }
+  get weakness() {
+    return this.#weakness.toLowerCase().trim();
+  }
 }
--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,10 +1,13 @@
-import { Choice } from "class.js";
+import { Item } from "./class.js";
+import { spinBot, stopBot } from "./botSelection.js";
 
+
+const box = document.getElementById("alert-box");
 const choices = document.querySelectorAll("img");
 const timerEl = document.getElementById("countdown-field");
-const rock = new Choice("rock", "paper");
-const scissors = new Choice("scissors", "rock");
-const paper = new Chhoice("paper", "scissors");
+const rock = new Item("rock", "paper");
+const scissors = new Item("scissors", "rock");
+const paper = new Item("paper", "scissors");
 const allChoices = [rock, scissors, paper];
 let humanChoice;
 
@@ -15,6 +18,9 @@ const score = {
 
 let timer = 3;
 let timerId;
+
+spinBot();
+box.innerHTML="";
 
 function startTimer() {
   if (timerId) clearInterval(timerId);
@@ -37,39 +43,36 @@ choices.forEach((choice) => {
     humanChoice = allChoices.find(
       (item) => e.target.getAttribute("value") === item.name
     );
+    console.log(humanChoice);
 
     let botChoice = allChoices[Math.floor(Math.random() * allChoices.length)];
+    stopBot(botChoice.name);
 
-    if (humanChoice.beaten == botChoice.name) {
-      score.botScore++;
-    }
-    if (humanChoice.name == botChoice.beaten) {
-      score.humanScore++;
-    }
-    if (humanChoice.name == botChoice.name) {
-    }
+      if (humanChoice.weakness == botChoice.name) {
+        box.innerHTML="Computer gets a point"
+        score.botScore++;
+      }
+      if (humanChoice.name == botChoice.weakness) {
+        box.innerHTML="Human gets a point"
+        score.humanScore++;
 
-    if (score.botScore == 3 || score.humanScore == 3) {
-      score.botScore = 0;
-      score.humanScore = 0;
+      }
+    
+    if (score.botScore == 2) {
+      box.innerHTML="Computer Wins";
+      setTimeout(() => {  score.humanScore = 0; score.botScore = 0; box.innerHTML="" }, 1000);
+      
+    } else if (score.humanScore == 2) {
+      box.innerHTML="Human Wins!";
+      setTimeout(() => {  score.humanScore = 0; score.botScore = 0; box.innerHTML = ""}, 1000);
     }
-
     updateScore();
-    startTimer();
-
+    setTimeout(() => {
+      startTimer();
+      spinBot();
+    }, 2000);
     humanChoice = null;
   });
 });
 
-const images = [
-  "sources/Papir.png",
-  "sources/scissors.png",
-  "sources/Rock.png",
-];
-const botImage = document.getElementById("bot");
-function roulette() {
-  let index = images.length - 1;
-  setInterval(() => {
-    botImage.src = images[index--];
-  });
-}
+
